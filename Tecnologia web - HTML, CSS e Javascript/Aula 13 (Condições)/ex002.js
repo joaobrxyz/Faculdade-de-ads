1f0b583d5022c3a2ebfc52a5d40f30ef194fa0b6
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-09-16 09:14:16 -0300

    Aula 13

---
+++ Tecnologia web - HTML, CSS e Javascript/Aula 13 (Condições)/ex002.js
@@ -0,0 +1,6 @@
+function verificar(){
+    let idade = document.form1.txtIdade.value // usando o name
+    let idade1 = document.getElementById("txtIdade").value
+    alert(idade)
+    alert(idade1)
+}
