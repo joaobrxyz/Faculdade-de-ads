e0d12f0af0fee813dc81a04dbfc317c67175998c
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-09-17 08:50:27 -0300

    Add files via upload

---
+++ Tecnologia web - HTML, CSS e Javascript/Aula 14 (BOM, cookie e temporizadores)/ex001.js
@@ -0,0 +1,8 @@
+let largura = screen.width
+let altura = screen.height
+let larguraUtil = screen.availWidth
+let alturaUtil = screen.availHeight
+document.write(`Largura da tela: ${largura} pixels<br>`)
+document.write(`Altura da tela: ${altura} pixels<br><br>`)
+document.write(`Largura da tela: ${larguraUtil} pixels<br>`)
+document.write(`Altura da tela: ${alturaUtil} pixels`)
