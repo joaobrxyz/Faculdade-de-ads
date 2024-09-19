5b43f08e454224b2b4ef74f93f277dc64a6c5200
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-09-19 09:01:26 -0300

    Add files via upload

---
+++ Linguagem C/Aula 09 (Estrutura de repetição)/ex005.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+int main(){	
+    setlocale(LC_ALL,"Portuguese");
+    int cont, num, n;
+    srand(time(NULL)); //semente para gerar uma sequencia de números 
+    printf("Informe a quantidade numeros para MegaSena: n = ");
+    scanf("%d",&n);
+    printf("Os numeros gerados para MegaSena:\n");   
+    /*cont = 1;
+    while (cont <= n){    	
+    	// gerar um número aleatório para mega sena 
+    	num = 1 + rand()%60 ;
+    	printf("%d ",num);
+    	cont++;  //cont = cont + 1;  	    	
+    }  */
+    for (cont = 1; cont <= n; cont++){
+    	num = 1 + rand()%60 ;
+    	printf("%d ",num);
+    }
+    printf("\n");
+    system("pause");
+    return 0;    
+}
