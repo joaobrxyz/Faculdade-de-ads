5953ed43bb273879e22ad097fc5ccf6d60d30d1f
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-18 08:52:03 -0300

    Add files via upload

---
+++ Linguagem Python/Aula 01 (Primeiros passos)/ex001.py
@@ -0,0 +1,25 @@
+nome = 'Neymar'
+print(nome)
+
+sobrenome = "Júnior"
+print(sobrenome)
+
+numero = 12
+print(7+60)
+print(67 - numero)
+
+salario = 13456.32
+ok = True
+
+numero = 'Aqui é um texto'
+print(type(numero))
+numero = 34
+print(type(numero))
+print(type(salario))
+print(type(ok))
+
+lista = [34, 2, -3, 0, 45]
+print(type(lista))
+
+endereço = 'Logo ali'
+print(endereço)
