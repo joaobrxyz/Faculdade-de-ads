dd386d1dcd73957ae3f2042aaa222e8bec980adf
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-24 08:35:09 -0300

    Add files via upload

---
+++ Linguagem Python/Aula 02 (Comandos de entrada e saída)/ex001.py
@@ -0,0 +1,18 @@
+print('Hello world!')
+
+nome = 'João'
+#Aqui é uma linha de comentário
+print(nome, end='<final do print>')
+print(nome)
+print('Aqui é um texto')
+
+print('Felipe', 'Carlos', 'Paloma', 'Milena')
+print('Felipe', 'Carlos', 'Paloma', 'Milena', sep=' - ')
+
+print('Nome\tIdade\tEmail')
+print('Ana\t18\tana@eniac.edu.br')
+print('Bruno\t22\tbruno@eniac.edu.br')
+print('Luiz\t17\tluiz@eniac.edu.br')
+
+print( 123 * 5 )
+print( '123' * 5 )
