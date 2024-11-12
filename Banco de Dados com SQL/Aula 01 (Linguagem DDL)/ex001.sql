95191cbe07f861269cc53c5fb3299414e9ae2e1f
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-11-12 07:50:21 -0300

    Add files via upload

--- Banco de Dados com SQL/Aula 01 (Linguagem DDL)/ex001.sql
+++ Banco de Dados com SQL/Aula 01 (Linguagem DDL)/ex001.sql
@@ -1,16 +1,7 @@
-create database escola;
-use escola;
+-- Alterar a tabela de alunos, adicionando a coluno status do aluno.
+alter table alunos add column status varchar(10);
 
-create table alunos (
-	ra int primary key auto_increment,
-    nome varchar(50) not null,
-    cpf varchar(18) unique,
-    endereco varchar(50),
-    bairro varchar(50),
-    cidade varchar(50),
-    uf char(2),
-    cep char(8),
-    email varchar(50) not null unique
-);
+alter table alunos modify column endereco varchar(100);
 
-drop table alunos;
+alter table alunos add column curso varchar(50);
+alter table alunos drop column curso;
