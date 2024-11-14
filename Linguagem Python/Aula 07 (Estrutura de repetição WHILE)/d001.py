06604228a4104f29922d6703f583ce73b506216c
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-11-14 08:52:02 -0300

    Add files via upload

---
+++ Linguagem Python/Aula 07 (Estrutura de repetição WHILE)/d001.py
@@ -0,0 +1,7 @@
+# Crie um programa que mostre na tela os números de 1 até 100 em ordem decrescente.
+
+cont = 100
+
+while cont >= 1:
+    print(cont)
+    cont -= 1
---
+++ Linguagem Python/Aula 07 (Estrutura de repetição WHILE)/d002.py
@@ -0,0 +1,9 @@
+# Faça um programa que some os números entre 1 até 100, e no final mostre o resultado da soma.
+
+cont = 1
+soma = 0
+
+while cont <= 100:
+    soma += cont
+    cont += 1
+print(soma)
