6ffb414da423907f0c1dbab94b50a4040ab2e0a8
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-11-22 08:49:15 -0300

    Aula 08

---
+++ Linguagem Python/Aula 08 (Estrutura de repetição for)/ex001.py
@@ -0,0 +1,6 @@
+lista = [3, 7, 1, 10, -2, 5]
+
+lista.sort(reverse=True)
+
+for item in lista :
+    print(item)
---
+++ Linguagem Python/Aula 08 (Estrutura de repetição for)/ex004.py
@@ -0,0 +1,4 @@
+aluno = {"nome" : "João", "Idade" : 20, "cpf": "123.456.789-10", "curso": "Gestão TI"}
+
+for chave, valor in aluno.items():
+    print(f"{chave}: {valor}")
