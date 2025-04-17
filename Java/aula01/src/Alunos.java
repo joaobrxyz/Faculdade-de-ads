7a48c47c240e4667f0740cd222176c44a717679d
DeyvideDhyonata <149846523+joaobrxyz@users.noreply.github.com>
2025-04-17 08:54:52 -0300

    aula 01

---
+++ Java/aula01/src/Alunos.java
@@ -0,0 +1,10 @@
+public class Alunos {
+    // Propriedades
+    int ra;
+    String nome;
+
+    // Métodos
+    public void mostrarDados() {
+        System.out.println("Executando o método mostrarDados");
+    }
+}
---
+++ Java/aula01/src/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+        // Criar os objetos
+        Alunos objAluno1 = new Alunos();
+        objAluno1.ra = 123;
+        objAluno1.nome = "Godofredo";
+        objAluno1.mostrarDados();
+
+        Alunos objAluno2 = new Alunos();
+        objAluno2.ra = 456;
+        objAluno2.nome = "Merdonio";
+        objAluno2.mostrarDados();
+    }
+}
