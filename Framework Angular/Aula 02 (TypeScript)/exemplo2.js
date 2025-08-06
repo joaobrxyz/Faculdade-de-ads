b583bf49702bbcbd95e0ac8aebf341dac4b1f9f7
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-08-06 08:44:28 -0300

    Aula 02

---
+++ Framework Angular/Aula 02 (TypeScript)/exemplo2.js
@@ -0,0 +1,13 @@
+var nome = "João";
+var idade = 20;
+var salario = 1234.56;
+var numHex = 0xf00d;
+var numOctal = 484;
+var numBinario = 10;
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Salário: " + salario);
+console.log("Número Hexadecimal: " + numHex);
+console.log("Número Octal: " + numOctal);
+console.log("Número Binário: " + numBinario);
+// Para compilar: tsc (nome do arquivo).ts
---
+++ Framework Angular/Aula 02 (TypeScript)/exemplo2.ts
@@ -0,0 +1,16 @@
+var nome:string = "João" ;
+var idade:number = 20 ;
+var salario:number = 1234.56 ;
+
+var numHex:number = 0xf00d ;
+var numOctal:number = 0o744 ;
+var numBinario:number = 0b1010 ;
+
+console.log("Nome: " + nome) ;
+console.log("Idade: " + idade) ;
+console.log("Salário: " + salario) ;
+console.log("Número Hexadecimal: " + numHex) ;
+console.log("Número Octal: " + numOctal) ;
+console.log("Número Binário: " + numBinario) ;
+
+// Para compilar: tsc (nome do arquivo).ts
