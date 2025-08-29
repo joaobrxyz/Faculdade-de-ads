7a256aeef96cfc737de92ca2f2eccdc387da2806
joaobrxyz <149846523+joaobrxyz@users.noreply.github.com>
2025-08-29 08:52:56 -0300

    Aula 08

---
+++ Framework Angular/angular4/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Evento1 } from './componentes/evento1/evento1';
+import { Evento2 } from './componentes/evento2/evento2';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Evento1, Evento2],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular4');
+}
